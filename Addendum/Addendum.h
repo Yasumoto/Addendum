af4da96dacf65e993ccc0688f3dab9fe2ac2e4a2
Joe Smith <jmsmith@slack-corp.com>
2016-12-15 14:15:35 -0800

    Initial Commit

---
+++ Addendum/Addendum.h
@@ -0,0 +1,19 @@
+//
+//  Addendum.h
+//  Addendum
+//
+//  Created by Joseph Mehdi Smith on 12/15/16.
+//  Copyright © 2016 Joe Smith. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Addendum.
+FOUNDATION_EXPORT double AddendumVersionNumber;
+
+//! Project version string for Addendum.
+FOUNDATION_EXPORT const unsigned char AddendumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Addendum/PublicHeader.h>
+
+
